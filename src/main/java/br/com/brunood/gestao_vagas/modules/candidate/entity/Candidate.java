fa4e0dc60baa0b2aed9647be17848e5860464933
brunood07 <brunood07@gmail.com>
2023-11-09 21:30:06 -0300

    feat: creating candidate table

--- src/main/java/br/com/brunood/gestao_vagas/modules/candidate/entity/Candidate.java
+++ src/main/java/br/com/brunood/gestao_vagas/modules/candidate/entity/Candidate.java
@@ -1,17 +1,28 @@
 package br.com.brunood.gestao_vagas.modules.candidate.entity;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
+@Entity(name = "candidate")
 public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private String name;
 
     @NotBlank()
@@ -25,4 +36,7 @@ public class Candidate {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createAt;
 }
